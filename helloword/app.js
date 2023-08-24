61ca5caf28d1748bbf77bfd1a005319a1bd95fbc
Kiran A <13688319+kiranarsam@users.noreply.github.com>
2023-08-24 15:37:35 +0530

    Delete helloword directory

--- helloword/app.js
+++
@@ -1,7 +0,0 @@
-var message = 'Hello, TypeScript!';
-// console.log(message);
-// create heading element h1
-var heading = document.createElement('h1');
-heading.textContent = message;
-// append h1 element to the document
-document.body.appendChild(heading);
